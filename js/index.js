7adca96662c6444ca39c6d832ee6c43b2242091c
monikatamsoy <ssumugan007@gmail.com>
2018-04-17 22:07:10 +0530

    add grid

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
-var app = angular.module(app, [])
+var app = angular.module("app", [])
 
 app.controller("imageGrid", function($scope) {
-  $scope.imageArr = Array(20).join().split(',').map(function(a) {
-    return this.i++
-  }, {i:1})
+
+    $scope.imageArr = []
+    for (var i = 1; i <= 20; i++) {
+        $scope.imageArr.push(i)
+    }
+
+    $scope.loadMore = function() {
+        var arrLength = $scope.imageArr.length
+        for (var i = arrLength+1; i <= arrLength + 20; i++ ) {
+            $scope.imageArr.push(i)
+        }
+    }
+    console.log($scope.imageArr)
 })
